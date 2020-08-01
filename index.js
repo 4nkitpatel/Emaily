6f67c34f58dacd5506a6b0ac3a836aa9435a79d8
4nkitpatel <4nkitpatel@gmail.com>
2020-08-01 23:28:57 +0530

    changed greeting

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express"); //commonJS module system node uses not that ES module (import this from that)
-const app = express()
+const app = express();
 
 app.get("/", (req, res) => {
-    res.send({hi : "there"})
-})
+  res.send({ bye: "buddy" });
+});
 
 // if there is not env var define by herouk use 5000 so we will use 5000 in dev but in production herouk internalyy det this env port to its convinience
-const PORT = process.env.PORT || 5000
-app.listen(PORT)
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
